f1c72fc141cd6677f711c18ef59f3ab3a4ea83d7
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-08-18 10:02:43 -0300

    calculator buttons

--- Tkinter Course - Create Graphic User Interfaces in Python Tutorial/5: simple calculator/calculator.py
+++ Tkinter Course - Create Graphic User Interfaces in Python Tutorial/5: simple calculator/calculator.py
@@ -6,20 +6,24 @@ root.title("Calculator")
 entry = Entry(root, width=35, borderwidth=5)
 entry.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
-def button_add():
+def button_click():
     return
 
 #defining buttons
-button_1 = Button(root, text="1", padx=40, pady=20, command=button_add)
-button_2 = Button(root, text="2", padx=40, pady=20, command=button_add)
-button_3 = Button(root, text="3", padx=40, pady=20, command=button_add)
-button_4 = Button(root, text="4", padx=40, pady=20, command=button_add)
-button_5 = Button(root, text="5", padx=40, pady=20, command=button_add)
-button_6 = Button(root, text="6", padx=40, pady=20, command=button_add)
-button_7 = Button(root, text="7", padx=40, pady=20, command=button_add)
-button_8 = Button(root, text="8", padx=40, pady=20, command=button_add)
-button_9 = Button(root, text="9", padx=40, pady=20, command=button_add)
-button_0 = Button(root, text="0", padx=40, pady=20, command=button_add)
+button_1 = Button(root, text="1", padx=40, pady=20, command=button_click)
+button_2 = Button(root, text="2", padx=40, pady=20, command=button_click)
+button_3 = Button(root, text="3", padx=40, pady=20, command=button_click)
+button_4 = Button(root, text="4", padx=40, pady=20, command=button_click)
+button_5 = Button(root, text="5", padx=40, pady=20, command=button_click)
+button_6 = Button(root, text="6", padx=40, pady=20, command=button_click)
+button_7 = Button(root, text="7", padx=40, pady=20, command=button_click)
+button_8 = Button(root, text="8", padx=40, pady=20, command=button_click)
+button_9 = Button(root, text="9", padx=40, pady=20, command=button_click)
+button_0 = Button(root, text="0", padx=40, pady=20, command=button_click)
+button_add = Button(root, text="add", padx=33, pady=20, command=button_click)
+button_sub = Button(root, text="sub", padx=33, pady=20, command=button_click)
+button_eqs = Button(root, text="=", padx=100, pady=20, command=button_click)
+button_clr = Button(root, text="Clear", padx=96, pady=20, command=button_click)
 
 #putting buttons on the screen
 button_1.grid(row=1, column=0)
@@ -35,5 +39,10 @@ button_8.grid(row=3, column=1)
 button_9.grid(row=3, column=2)
 
 button_0.grid(row=4, column=0)
+button_sub.grid(row=4, column=1)
+button_add.grid(row=4, column=2)
+
+button_clr.grid(row=5, column=0, columnspan=3)
+button_eqs.grid(row=6, column=0, columnspan=3)
 
 root.mainloop()
