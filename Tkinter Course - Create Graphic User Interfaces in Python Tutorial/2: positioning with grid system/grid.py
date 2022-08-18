7f44bf8fd393543f6e388b8ce683df8b3d996b8a
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-08-18 09:12:34 -0300

    2 grid system

--- Tkinter Course - Create Graphic User Interfaces in Python Tutorial/2: positioning with grid system/grid.py
+++ Tkinter Course - Create Graphic User Interfaces in Python Tutorial/2: positioning with grid system/grid.py
@@ -4,10 +4,8 @@ root = Tk()
 
 myLabel1 = Label(root, text="Hello World")
 myLabel2 = Label(root, text="My name is Jefferson")
-myLabel3 = Label(root, text="-------------")
 
 myLabel1.grid(row=0, column=0)
-myLabel3.grid(row=1, column=2) #relative to each other
-myLabel2.grid(row=2, column=5)
+myLabel2.grid(row=1, column=1)
 
 root.mainloop()
