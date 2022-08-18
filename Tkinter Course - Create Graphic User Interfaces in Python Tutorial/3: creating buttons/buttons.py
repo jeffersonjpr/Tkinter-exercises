21328b207ecd7f1d1d5dc24e4f3b140fd9399d22
jeffersonjpr <jefferson_michael407@hotmail.com>
2022-08-18 09:17:26 -0300

    3: buttons padx pady

--- Tkinter Course - Create Graphic User Interfaces in Python Tutorial/3: creating buttons/buttons.py
+++ Tkinter Course - Create Graphic User Interfaces in Python Tutorial/3: creating buttons/buttons.py
@@ -1,8 +1,12 @@
 from tkinter import *
 
+def myClick():
+    myLabel = Label(root, text="Look! I cklicked a button!!")
+    myLabel.pack()
+
 root = Tk()
 
-myButton = Button(root, text="Click Me", state=DISABLED)
+myButton = Button(root, text="Click Me", padx=30, pady=5)
 myButton.pack()
 
 root.mainloop()
